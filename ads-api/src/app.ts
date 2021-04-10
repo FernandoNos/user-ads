08ca8b2b261107a81d98c926b8163a8b30953935
Fernando <nosfernandos@gmail.com>
2021-04-10 16:56:43 -0300

    exposing access to api

--- ads-api/src/app.ts
+++ ads-api/src/app.ts
@@ -1,10 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express';
-
+import {debug} from "debug";
+import {Database} from "./database";
+debug('ads-api');
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-});
+const {PORT} = process.env;
+
+(async()=>{
+  module.exports.MongoClient = await new Database().getDbConenction()
+
+  app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!');
+  });
+  app.listen(PORT, () => {
+    debug.log(`server is listening on ${PORT}`);
+  });
+})()
+    .catch(error => {
+      debug(`Error starting up application : ${error.message}`)
+      process.exit(0)
+    })
+
+
+
--- ads-api/src/database/index.ts
+++ ads-api/src/database/index.ts
@@ -1,24 +1,21 @@
 import { Db, MongoClient } from "mongodb";
+import {debug} from 'debug'
+debug('configs:mongo');
 
 export class Database {
-  mongoClient: MongoClient;
-
-  constructor() {
-    this.mongoClient = null;
-  }
-
-  getDbConenction(): Promise<Db> {
-    const { MONGO_PASSWORD, MONGO_HOSTS, MONGO_USERNAME, MONGO_DATABASE } = process.env;
-    const URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTS}?replicaSet=repDB`;
+  getDbConenction(): Promise<Db | null> {
+    const { MONGO_HOSTS } = process.env;
+    // const URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTS}?replicaSet=repDB`;
+    const URL = `mongodb://${MONGO_HOSTS}/mongo`;
 
     return MongoClient.connect(URL, { useUnifiedTopology: true })
       .then((db) => {
-        console.info(`MongoDb: Success connecting to db`);
-        return db.db(MONGO_DATABASE);
+        debug.log(`MongoDb: Success connecting to db`);
+        return db.db('mongo');
       })
       .catch((error) => {
-        console.error(`MongoDb: Error connecting to db ${error} ${error.message ? error.message : ""}`);
-        return null;
+        debug.log(`MongoDb: Error connecting to db ${error} ${error.message ? error.message : ""}`);
+        throw error
       });
   }
 }
