21a333e9ad3cd6f37139e77136bc702f3a3ad28f
Fernando <nosfernandos@gmail.com>
2021-04-12 21:11:29 -0300

    including apis for admins

--- security-api/src/adapters/input/routes/SecurityRoutes.ts
+++ security-api/src/adapters/input/routes/SecurityRoutes.ts
@@ -6,13 +6,14 @@ import {isAdmin} from "../middlewares/IsAdminMiddleware";
 import {paramToLocals} from "../middlewares/ParamToLocals";
 const router = express.Router();
 
-
+//APIs for users to register and handle their own data
 router.post("/register", register);
 router.post("/login",[login,generateJWT])
 router.patch("/user", [readJWT, updateUser])
 router.delete("/user", [readJWT, deleteUser])
 router.get("/user", getUsers)
 
+//APIs for admins to update and delete users
 router.delete("/user/:uuid", [readJWT, isAdmin, paramToLocals("user"),deleteUser])
 router.patch("/user/:uuid", [readJWT, isAdmin, paramToLocals("user"),updateUser])
 export default router
