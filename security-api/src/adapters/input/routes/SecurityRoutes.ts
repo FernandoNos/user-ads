94a12964c3148d9d59fa85b52ded54669d024259
Fernando <nosfernandos@gmail.com>
2021-04-12 21:09:07 -0300

    including the option to delete user

--- security-api/src/adapters/input/routes/SecurityRoutes.ts
+++ security-api/src/adapters/input/routes/SecurityRoutes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import {register, login, updateUser, deleteUser} from "../controllers/SecurityController";
 import {generateJWT, readJWT} from "../middlewares/JWTMiddleware";
 import {getUsers} from "../controllers/UsersController";
+import {isAdmin} from "../middlewares/IsAdminMiddleware";
+import {paramToLocals} from "../middlewares/ParamToLocals";
 const router = express.Router();
 
 
@@ -10,4 +12,6 @@ router.post("/login",[login,generateJWT])
 router.patch("/user", [readJWT, updateUser])
 router.delete("/user", [readJWT, deleteUser])
 router.get("/user", getUsers)
+
+router.delete("/user/:uuid", [readJWT, isAdmin, paramToLocals("user"),deleteUser])
 export default router
