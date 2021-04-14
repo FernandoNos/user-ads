fcf746ca62581280dca7a2d4f204759a3e55dd1c
Fernando <nosfernandos@gmail.com>
2021-04-14 00:00:52 -0300

    changing routes

--- security-api/src/adapters/input/routes/SecurityRoutes.ts
+++ security-api/src/adapters/input/routes/SecurityRoutes.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import {register, login, updateUser, deleteUser} from "../controllers/SecurityController";
 import {generateJWT, readJWT} from "../middlewares/JWTMiddleware";
-import {getUsers} from "../controllers/UsersController";
-import {isAdmin} from "../middlewares/IsAdminMiddleware";
-import {paramToLocals} from "../middlewares/ParamToLocals";
 const router = express.Router();
 
 //APIs for users to register and handle their own data
