5b9fbd5a193a312b744d8d7f7b9f15a2cf550921
Fernando <nosfernandos@gmail.com>
2021-04-13 23:28:20 -0300

    refactory

--- ads-api/src/adapters/input/controllers/ProductsController.ts
+++ ads-api/src/adapters/input/controllers/ProductsController.ts
@@ -5,14 +5,20 @@ import {build} from "../../../core/use-cases/models/ProductModel";
 import _ from 'lodash'
 
 export async function get(request: Request, response: Response){
-        getProducts(request.params,request.query)
-            .then(product => response.send(product))
-            .catch(error => response.status(error.status ?? 500).send(error.message))
+    try {
+        const product = await getProducts(request.params,request.query)
+        response.send(product)
+    }catch(error){
+        response.status(error.status ?? 500).send(error.message)
+    }
 }
 
 export async function create(request: Request, response: Response){
+    try {
         const productCreationRequest = validateProductCreationRequest(request.body)
-        createProduct(build(productCreationRequest))
-            .then(result => response.send(result))
-            .catch(error => response.status(error.status ?? 500).send(error.message) )
+        const result = await createProduct(build(productCreationRequest))
+        response.send(result)
+    }catch(error){
+        response.status(error.status ?? 500).send(error.message)
+    }
 }
