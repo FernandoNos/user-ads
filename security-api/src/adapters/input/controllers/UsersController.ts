993e9c48b8bbef34a9c00fd46a4b0535bec01c4b
Fernando <nosfernandos@gmail.com>
2021-04-13 22:47:59 -0300

    updating check

--- security-api/src/adapters/input/controllers/UsersController.ts
+++ security-api/src/adapters/input/controllers/UsersController.ts
@@ -4,7 +4,7 @@ import * as UserActionsUseCase from '../../../core/use-cases/UserActionsUseCase'
 
 export async function getUsers(request: Request, response: Response){
     try{
-        return response.send(await UserActionsUseCase.getUsers(request.params))
+        return response.send(await UserActionsUseCase.getUsers(request.query))
     }catch (error){
         response.status(error.status ?? 500).send(error.message)
     }
