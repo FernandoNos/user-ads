6592fa76e7b8ed9cfddd063de7aed080a63a63cf
Fernando <nosfernandos@gmail.com>
2021-04-14 23:01:44 -0300

    including logs

--- users-api/src/adapters/input/controllers/SecurityController.ts
+++ users-api/src/adapters/input/controllers/SecurityController.ts
@@ -2,13 +2,9 @@ import {NextFunction, Request, Response} from 'express'
 import {RegisterRequestModel} from "./models/RegisterRequestModel";
 import * as RegistrationUseCase from '../../../core/use-cases/SecurityUseCase'
 import * as UserLoginUseCase from '../../../core/use-cases/SecurityUseCase'
-import * as UserActions from '../../../core/use-cases/UserUseCase'
-import {debug} from "debug";
 import * as UserModel from "../../../core/use-cases/models/UserModel";
 import * as UserLoginModel from "../../../core/use-cases/models/UserLoginModel";
 import {LoginRequestModel} from "./models/LoginRequestModel";
-import _ from 'lodash'
-debug('users-api:SecurityController');
 
 export async function register(request: Request, response: Response){
     try {
@@ -16,7 +12,7 @@ export async function register(request: Request, response: Response){
         await RegistrationUseCase.register(UserModel.build(reqBody))
         response.sendStatus(201)
     }catch(error){
-        debug(`Error requesting registration ${error.message} ${error.stackTrace}`)
+        console.error(`Error requesting registration ${error.message} ${error.stackTrace}`)
         response.status(error.status ?? 500).send({message: error.message})
     }
 }
@@ -29,7 +25,7 @@ export async function login(request: Request, response: Response, next: NextFunc
         response.locals.user = result
         next()
     }catch(error){
-        debug(`Error requesting registration ${error.message} ${error.stackTrace}`)
+        console.error(`Error requesting registration ${error.message} ${error.stackTrace}`)
         response.status(error.status ?? 500).send({message: error.message})
     }
 }
